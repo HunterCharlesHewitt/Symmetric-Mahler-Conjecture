8b61462ce6dc77c9dd7cbdfb719cbb809878e16b
Nathan Derhake <nathanderhake@gmail.com>
2025-04-16 04:06:11 -0700

    refactor: Add ability to visualize second polytope

--- visualizer.py
+++ visualizer.py
@@ -2,7 +2,7 @@ from scipy.spatial import HalfspaceIntersection
 import matplotlib.pyplot as plt
 import numpy as np
 
-def visualize_polytope(polytope, points = []):
+def visualize_polytope(polytope, points = [], second_polytope = None):
     dim = len(polytope[0])
     if dim != 2:
         raise Exception("Can not visualize non two dimentional polytopes.")
@@ -24,9 +24,19 @@ def visualize_polytope(polytope, points = []):
 
     plt.fill(*zip(*sorted_vertices), alpha=0.5, edgecolor='green')
     plt.scatter(*zip(*sorted_vertices), color='green')
-    plt.scatter(*zip(*points), color='red')
+    if points:
+        plt.scatter(*zip(*points), color='red')
     plt.gca().set_aspect('equal')
     plt.title("Convex Polygon (Sorted via Interior Point)")
     plt.grid(True)
+
+    if second_polytope is not None:
+        halfspaces2 = np.array([np.append(vector, -1) for vector in second_polytope])
+
+        hs2 = HalfspaceIntersection(halfspaces2, interior_point)
+        vertices2 = sorted(hs2.intersections, key=angle_from_interior)
+
+        plt.fill(*zip(*vertices2), alpha=0.5, edgecolor='orange')
+        plt.scatter(*zip(*vertices2), color='orange')
     plt.show()
     
