e6802048d13a791b5fd83f7c075ddc4dc931f91d
Nathan Derhake <nathanderhake@gmail.com>
2025-05-07 12:58:38 -0700

    refactor: Add stop_num posibility to set_billiard_system_ratio

--- Project/Services/Calculation/RatioService.py
+++ Project/Services/Calculation/RatioService.py
@@ -3,11 +3,11 @@ import math
 from Project.Services.Calculation import VolumeService, CapacityService
 
 
-def set_billiard_system_ratio(billiard_system):
+def set_billiard_system_ratio(billiard_system, stop_num=0):
     K = billiard_system.K
     T = billiard_system.T
     vol = VolumeService.get_volume_k_metric(K=K, T=T)
-    capacity = CapacityService.get_capacity(T=T, K=K, volume_k_metric=vol)
+    capacity = CapacityService.get_capacity(T=T, K=K, volume_k_metric=vol, stop_num=stop_num)
     billiard_system.volume_k_metric = vol
     billiard_system.capacity_k_of_t = capacity
     billiard_system.ratio = math.pow(capacity.length, T.dim) / vol
