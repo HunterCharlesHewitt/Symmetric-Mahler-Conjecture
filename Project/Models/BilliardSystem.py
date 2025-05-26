d1eec73f4a6c9807a5f540ffe65c0ab6e6a34b30
Nathan Derhake <nathanderhake@gmail.com>
2025-05-25 19:53:59 -0700

    feat: Write method in BilliardSystem to get dual billard system

--- Project/Models/BilliardSystem.py
+++ Project/Models/BilliardSystem.py
@@ -1,3 +1,5 @@
+from Project.Services.Calculation.VolumeService import get_polar_dual
+
 class BilliardSystem:
     def __init__(self, K, T):
         self.K = K
@@ -6,3 +8,6 @@ class BilliardSystem:
         self.volume_k_metric = None
         self.capacity_k_of_t = None
         self.trajectory = None
+    
+    def get_dual_billiard_system(self):
+        return BilliardSystem(T = get_polar_dual(self.K), K = get_polar_dual(self.T))
