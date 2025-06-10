f8e5cfec481fa4d672dc739272828eff2d68ae6f
Hunte <hchewitt@usc.edu>
2025-06-10 02:43:44 -0700

    began making it ever so slightly more flexible, still working

--- Project/Services/Output/FinslerTrajectoryVisualizerService.py
+++ Project/Services/Output/FinslerTrajectoryVisualizerService.py
@@ -6,9 +6,6 @@ from matplotlib.animation import PillowWriter
 from matplotlib.patches import Polygon
 from moviepy import VideoFileClip
 
-def func(x):
-    return np.sin(x) * 3
-
 
 def draw_T():
     square = plt.Rectangle((-3, -3), 6, 6, facecolor='white', edgecolor="red")
@@ -41,16 +38,37 @@ def set_axs_min_max(local_axs, axs_min, axs_max):
         ax.set_ylim([axs_min, axs_max])
 
 
+def get_slope():
+    pass
+
+
+def draw_T_bounce(new_bounce_starting_position, slope):
+    point = [new_bounce_starting_position[0], new_bounce_starting_position[1]]
+    while T_shape.contains_point(axs[0].transData.transform((point[0], point[1]))):
+        add_frame_for_axis(axs_1_pts, point[0], point[1], xlist_T, ylist_T)
+        if slope < 0:
+            point[0] -= X_VALUE_CHANGE_RATE
+        elif slope > 0:
+            point[0] += X_VALUE_CHANGE_RATE
+        else:
+            point[1] += X_VALUE_CHANGE_RATE
+            continue
+        point[1] = slope * (point[0] - new_bounce_starting_position[0]) + new_bounce_starting_position[1]
+    return [xlist_T[-1], ylist_T[-1]]
+
+
 if __name__ == '__main__':
     # Size Parameters
     FRAMES_PER_SECOND = 30
     AXIS_MIN = -5
     AXIS_MAX = 5
+    X_VALUE_CHANGE_RATE = 3 / 70
     STARTING_T_POSITION = [0, -3]
     STARTING_T_ANGLE = 150
     STARTING_K_POSITION = (-1.5, 1.5 * tan(np.pi / 3) - 3)
 
     # K_NORMAL_SLOPES = [inf,sqrt(3)/3, -sqrt(3)/3]
+    # T_NORMAL_SLOPES = [0,-0, inf, -inf]
 
     # FIGURE SETUP
     fig, axs = plt.subplots(2, figsize=(5, 10))
@@ -66,34 +84,21 @@ if __name__ == '__main__':
     with writer.saving(fig, 'trajectory.gif', 100):
         xlist_T = []
         ylist_T = []
-        point = [STARTING_T_POSITION[0], STARTING_T_POSITION[1]]
-        while T_shape.contains_point(axs[0].transData.transform((point[0], point[1]))):
-            add_frame_for_axis(axs_1_pts, point[0], point[1], xlist_T, ylist_T)
-            if STARTING_T_ANGLE > 90:
-                point[0] -= 3 / 70  #TODO add or subtract based on angle
-            elif STARTING_T_ANGLE < 90:
-                point[0] += 3 / 70
-            else:
-                point[1] += 3 / 70
-                continue
-            m = np.tan(STARTING_T_ANGLE * np.pi / 180)
-            point[1] = m * (point[0]) + STARTING_T_POSITION[1]
-        #change starting_T_position_here
+        slope = np.tan(STARTING_T_ANGLE * np.pi / 180)
+        new_bounce_starting_position = draw_T_bounce(STARTING_T_POSITION, slope)
 
         xlist_K = []
         ylist_K = []
         for xval in np.linspace(np.cos(np.pi / 3) * 3 - 3, np.cos(np.pi / 3) * 3, 100):
             add_frame_for_axis(axs_2_pts, xval, np.sin(np.pi / 3) * 3 - 3, xlist_K, ylist_K)
 
-        # slope is now sqrt(3)/3
-        STARTING_T_POSITION = [-3, tan(30 * np.pi / 180) * 3 - 3]
-        point = [STARTING_T_POSITION[0], STARTING_T_POSITION[1]]
+        point = [new_bounce_starting_position[0], new_bounce_starting_position[1]]
         while T_shape.contains_point(axs[0].transData.transform((point[0], point[1]))):
             add_frame_for_axis(axs_1_pts, point[0], point[1], xlist_T, ylist_T)
             m = np.sqrt(3) / 3
-            point[0] += 3 / 70
-            point[1] = m * (point[0]) + (2*np.sqrt(3)-3)
-        STARTING_T_POSITION = [xlist_T[-1], ylist_T[-1]]
+            point[0] += X_VALUE_CHANGE_RATE
+            point[1] = m * (point[0] - new_bounce_starting_position[0]) + new_bounce_starting_position[1]
+        new_bounce_starting_position = [xlist_T[-1], ylist_T[-1]]
 
         xlist_K_new = []
         ylist_K_new = []
@@ -101,21 +106,21 @@ if __name__ == '__main__':
         for xval in np.linspace(np.cos(np.pi / 3) * 3, np.cos(np.pi / 3) * 3 - 3, 100):
             add_frame_for_axis(axs_2_pts_new, xval, np.sin(np.pi / 3) * 3 - 3, xlist_K_new, ylist_K_new)
 
-        point = [STARTING_T_POSITION[0], STARTING_T_POSITION[1]]
+        point = [new_bounce_starting_position[0], new_bounce_starting_position[1]]
         while T_shape.contains_point(axs[0].transData.transform((point[0], point[1]))):
             add_frame_for_axis(axs_1_pts, point[0], point[1], xlist_T, ylist_T)
             m = -1 * np.sqrt(3) / 3
-            point[0] -= 3 / 70
-            point[1] = m * (point[0] - STARTING_T_POSITION[0]) + STARTING_T_POSITION[1]
-        STARTING_T_POSITION = [xlist_T[-1], ylist_T[-1]]
+            point[0] -= X_VALUE_CHANGE_RATE
+            point[1] = m * (point[0] - new_bounce_starting_position[0]) + new_bounce_starting_position[1]
+        new_bounce_starting_position = [xlist_T[-1], ylist_T[-1]]
 
         for yval in np.linspace(np.sin(np.pi / 3) * 3 - 3, -3, 75):
             add_frame_for_axis(axs_2_pts_new, np.cos(np.pi / 3) * 3 - 3, yval, xlist_K_new, ylist_K_new)
 
-        for yval in np.linspace(STARTING_T_POSITION[1], -3, 75):
-            add_frame_for_axis(axs_1_pts, STARTING_T_POSITION[0], yval, xlist_T, ylist_T)
+        for yval in np.linspace(new_bounce_starting_position[1], -3, 75):
+            add_frame_for_axis(axs_1_pts, new_bounce_starting_position[0], yval, xlist_T, ylist_T)
 
-        axs_2_pts_new_new, = axs[1].plot([], [], 'k-')
+        axs_2_pts_new_new, = axs[1].plot([], [], color='#00FF00')
         xlist_K_new_new = []
         ylist_K_new_new = []
         for yval in np.linspace(-3, np.sin(np.pi / 3) * 3 - 3, 100):
